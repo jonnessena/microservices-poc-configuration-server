580f502f20698cc312aad894010ab0d9d02f6898
Jonnes Sena <jonnes.sena@globalhitss.com.br>
2019-12-31 16:45:11 -0300

    Configuration server projeto inicial.

---
+++ src/test/java/com/hitss/configserver/HitssConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitss.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitssConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
